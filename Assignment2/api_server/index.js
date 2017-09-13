1e4237aa27e09e167f8887e32ed699d205115d98
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2017-09-13 02:03:07 -0400

    End to end integration

--- Assignment2/api_server/index.js
+++ Assignment2/api_server/index.js
@@ -1,32 +1,55 @@
+var amqp = require('amqplib/callback_api');
+
 const express = require('express');
 const app = express();
 
 app.get('/secret_resource', function (req, res) {
-    var path = require('path')
-    res.sendFile(path.join(__dirname, 'index.html'))
+  var path = require('path')
+  res.sendFile(path.join(__dirname, 'index.html'))
 });
 
 app.get('/', function (req, res) {
-    res.redirect('/secret_resource')
+  res.redirect('/secret_resource')
 });
 
 app.get('/id/:id/password/:password/resource_id/:resource', function (req, res) {
-    const URL = req.url
-    const AUTH_SERVER_HOST = 'localhost'
-    const AUTH_SERVER_PORT = 5000
-    const util = require("util")
-    var queryString = util.format("http://%s:%d%s", AUTH_SERVER_HOST, AUTH_SERVER_PORT, URL)
-    console.log(queryString)
-    var request = require('request');
-    request(queryString, function (error, response, body) {
-      res.status(response.statusCode).send(body);
+  const userId = req.params.id
+  const password = req.params.password
+  const resourceId = req.params.resource
+
+  var reqObj = { 'userId': userId, 'password': password, 'resourceId': resourceId }
+  var reqStr = JSON.stringify(reqObj)
+
+  amqp.connect('amqp://localhost', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+      ch.assertQueue('', { exclusive: true }, function (err, q) {
+        var corr = Math.random().toString() +
+          Math.random().toString() +
+          Math.random().toString()
+
+        console.log(' [x] Requesting secret(%d)', reqStr);
+
+        ch.consume(q.queue, function (msg) {
+          if (msg.properties.correlationId === corr) {
+            var currResponse = msg.content.toString()
+            console.log(' [.] Got %s', currResponse);
+            res.send(currResponse)
+            setTimeout(function () { conn.close(); process.exit(0) }, 500);
+          }
+        }, { noAck: true });
+
+        ch.sendToQueue('authentication_queue',
+          new Buffer(reqStr.toString()),
+          { correlationId: corr, replyTo: q.queue });
+      });
     });
+  });
 });
 
 app.get('*', function (req, res) {
-    res.status(404).send('Incorrect URL')
+  res.status(404).send('Incorrect URL')
 });
 
 app.listen(3002, function () {
-    console.log("API server started on port 3002");
+  console.log("API server started on port 3002");
 });
--- Assignment2/authentication_service/rpc_server.py
+++ Assignment2/authentication_service/rpc_server.py
@@ -33,7 +33,7 @@ def authenticate_user(user_id, password):
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='rpc_queue')
+channel.queue_declare(queue='authentication_queue')
 
 
 def on_request(ch, method, props, body):
@@ -59,7 +59,7 @@ def on_request(ch, method, props, body):
 
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
+channel.basic_consume(on_request, queue='authentication_queue')
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
