caafbe7d6ba410faef3b345daf7ea6d2d2426506
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2017-09-18 11:23:48 -0400

    Sending message to remote RMQ server

--- Assignment2/authorization_service/index.js
+++ Assignment2/authorization_service/index.js
@@ -2,17 +2,7 @@
 
 var amqp = require('amqplib/callback_api');
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'authorization_user',
-  password : 'password',
-  database : 'authorization-db',
-  port     : 3306
-});
-connection.connect();
-
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect('amqp://new_user:password@149.165.169.11//', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'authorization_queue';
 
@@ -22,13 +12,17 @@ amqp.connect('amqp://localhost', function(err, conn) {
     ch.consume(q, function reply(msg) {
       var reqStr = msg.content.toString()
       var requestObj = JSON.parse(reqStr)
-      var userId = requestObj.id
-      var resourceId = requestObj.resource
+      var userId = requestObj.id.toString()
+      console.log('User ID -> ' + userId)
+      var resourceId = requestObj.resource.toString()
+      console.log('Resource ID -> ' + resourceId)
       isUserAuthorized(userId, resourceId, function (isAuthorized) {
         responseObj = {}
         responseObj.isAuthorized = isAuthorized
         if (isAuthorized) {
-          sendMessage(function(resourceString) {
+          console.log("Is authorized")
+          sendMessage(conn, ch, resourceId, function(resourceString) {
+            console.log("After callback from sendMessage")
             responseObj.responseCode = 200
             responseObj.resource = resourceString
             var response = JSON.stringify(responseObj)
@@ -38,6 +32,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
             ch.ack(msg);
           })
         } else {
+          console.log("Is not authorized")
           responseObj.responseCode = 401
           var response = JSON.stringify(responseObj)
           ch.sendToQueue(msg.properties.replyTo,
@@ -50,41 +45,51 @@ amqp.connect('amqp://localhost', function(err, conn) {
   });
 });
 
-function isUserAuthorized(id, resource, next) {
-  var queryString = "SELECT COUNT(*) AS cnt FROM `authorizations` WHERE user_id=? AND resource_id=?;";
-  connection.query(queryString, [id, resource], function (error, rows, fields) {
-    if (error) throw error;
-    if (rows[0].cnt >= 1) {
-      next(true);
-    } else {
-      next(false);
+function isUserAuthorized(userId, resourceId, next) {
+  const sqlite3 = require('sqlite3').verbose()
+  
+  let db = new sqlite3.Database('authorization_db.sqlite', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+      console.error(err.message)
     }
-  });
-};
+    console.log('Connected to the authorization database.')
+  })
 
-function sendMessage(next) {
-  amqp.connect('amqp://localhost', function(err1, conn1) {
-    conn1.createChannel(function(err1, ch1) {
-      ch1.assertQueue('', {exclusive: true}, function(err1, q) {
-        var corr = Math.random().toString() +
-                   Math.random().toString() +
-                   Math.random().toString()
-        var num = 1
-  
-        console.log(' [x] Requesting secret(%d)', num)
-  
-        ch1.consume(q.queue, function(msg) {
-          if (msg.properties.correlationId === corr) {
-            console.log(' [.] Got %s', msg.content.toString());
-            setTimeout(function() { conn1.close(); process.exit(0) }, 500);
-            next(msg.content.toString())
-          }
-        }, {noAck: true});
-  
-        ch1.sendToQueue('resource_queue',
-          new Buffer(num.toString()),
-          { correlationId: corr, replyTo: q.queue });
-      });
-    });
+  var queryString = 'SELECT COUNT(*) AS cnt FROM authorizations WHERE user_id = ? AND resource_id = ?;'
+
+  db.get(queryString, [userId, resourceId], (err, row) => {
+    if (err) {
+      console.error('Something bad happened')
+    }
+    if (row.cnt >= 1) {
+      next(true)
+    } else {
+      next(false)
+    }
+  })
+}
+
+function sendMessage(conn, ch, resourceID, next) {
+  ch.assertQueue('', {exclusive: true}, function(err1, q) {
+    var corr = Math.random().toString() +
+               Math.random().toString() +
+               Math.random().toString()
+
+    console.log(' [x] Requesting secret(%d)', resourceID)
+
+    ch.consume(q.queue, function(msg) {
+      if (msg.properties.correlationId === corr) {
+        console.log(' [.] Got %s', msg.content.toString());
+        setTimeout(function() {
+           // conn.close();
+           // process.exit(0) 
+        }, 500);
+        next(msg.content.toString())
+      }
+    }, {noAck: true});
+
+    ch.sendToQueue('resource_queue',
+      new Buffer(resourceID.toString()),
+      { correlationId: corr, replyTo: q.queue });
   });
 }
