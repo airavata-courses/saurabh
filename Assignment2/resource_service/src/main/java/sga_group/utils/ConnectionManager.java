8d2634ba9b14d43bf8f34120c3594e9844c38847
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2017-09-18 11:26:12 -0400

    Creating the in-memory database on system startup

--- Assignment2/resource_service/src/main/java/sga_group/utils/ConnectionManager.java
+++ Assignment2/resource_service/src/main/java/sga_group/utils/ConnectionManager.java
@@ -3,26 +3,35 @@ package sga_group.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionManager {
-/*	private static String dbUrl = 
-			"jdbc:mysql://localhost:3306/resource-db?autoReconnect=true&useSSL=false";
-	private static String dbUsername = "resource_user";
-	private static String dbPassword = "password";
-	private static Connection connection = null;
 
-	public static Connection getConnection() throws SQLException {
-	  if (connection == null) {
-	    connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-	  }
-	  return connection;
-	}*/
-  private static String dbUrl = 
-      "jdbc:mysql://localhost:3306/resource-db?autoReconnect=true&useSSL=false";
-  private static String dbUsername = "resource_user";
-  private static String dbPassword = "password";
+  private static final String DB_URL = "jdbc:sqlite::memory:";
+  private static Connection connection;
+  
+  static {
+    createDatabase();
+  }
+
+  public static void createDatabase() {
+    final String createTable = "CREATE TABLE resources (" +
+                                 "resource_id INTEGER, resource_string TEXT" + 
+                               ")";
+    final String insertRow = "INSERT INTO resources(resource_id, resource_string) " +
+                               "VALUES (1, 'resource1:secret1'), (2, 'resource2:secret2');";
+    try {
+      connection = DriverManager.getConnection(DB_URL);
+      try (Statement stmt = connection.createStatement()) {
+        stmt.execute(createTable);
+        stmt.execute(insertRow);
+      }
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 
   public static Connection getConnection() throws SQLException {
-    return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    return connection;
   }
 }
