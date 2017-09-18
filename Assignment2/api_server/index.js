e4eed7a6120c41d0470ce43ca97f031040e8f314
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2017-09-18 11:21:45 -0400

    Connecting to remote RMQ server

--- Assignment2/api_server/index.js
+++ Assignment2/api_server/index.js
@@ -20,21 +20,24 @@ app.get('/id/:id/password/:password/resource_id/:resource', function (req, res) 
   var reqObj = { 'userId': userId, 'password': password, 'resourceId': resourceId }
   var reqStr = JSON.stringify(reqObj)
 
-  amqp.connect('amqp://localhost', function (err, conn) {
+  amqp.connect('amqp://new_user:password@149.165.169.11//', function (err, conn) {
     conn.createChannel(function (err, ch) {
       ch.assertQueue('', { exclusive: true }, function (err, q) {
         var corr = Math.random().toString() +
           Math.random().toString() +
           Math.random().toString()
 
-        console.log(' [x] Requesting secret(%d)', reqStr);
+        console.log(' [x] Requesting secret(%s)', reqStr);
 
         ch.consume(q.queue, function (msg) {
           if (msg.properties.correlationId === corr) {
             var currResponse = msg.content.toString()
             console.log(' [.] Got %s', currResponse);
             res.send(currResponse)
-            setTimeout(function () { conn.close(); process.exit(0) }, 500);
+            setTimeout(function () {
+              conn.close();
+              // process.exit(0)
+            }, 500);
           }
         }, { noAck: true });
 
