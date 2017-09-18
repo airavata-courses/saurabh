b499156def284788b7e89c53d7504e975e40cb34
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2017-09-18 11:23:13 -0400

    Sending message to remote RMQ server

--- Assignment2/authentication_service/rpc_server.py
+++ Assignment2/authentication_service/rpc_server.py
@@ -1,37 +1,59 @@
 #!/usr/bin/env python
 import ast
 import pika
-import mysql.connector
+# import mysql.connector
 import json
 from rpc_client import AuthorizationClient
+from authenticate import is_authentic_user
 
 # Connecting to the MySQL database
-cnx = mysql.connector.connect(user='authentication_user',
-                              password='password',
-                              host='localhost',
-                              database='authentication-db')
-cursor = cnx.cursor()
+# cnx = mysql.connector.connect(user='authentication_user',
+#                               password='password',
+#                               host='localhost',
+#                               database='authentication-db')
+# cursor = cnx.cursor()
 
 # Server properties of the authorization server
 AUTHORIZATION_SERVER_HOST = "localhost"
 AUTHORIZATION_SERVER_PORT = 3000
 
 
-def authenticate_user(user_id, password):
-    """This function performs user authentication by checking the DB table 'authentications'
-    and forwards the request to authorization service
-    :param user_id: User ID
-    :param password: User password
-    :return: True if user is authentic, false otherwise
-    """
-    global cursor, AUTHORIZATION_SERVER_HOST, AUTHORIZATION_SERVER_PORT
-    query = "SELECT COUNT(*) AS cnt FROM `authentications` WHERE user_id=%s AND user_pwd=%s;"
-    cursor.execute(query, [user_id, password])
-    count = cursor.fetchone()[0]
-    return True if count >= 1 else False
+# def authenticate_user(user_id, password):
+#     """This function performs user authentication by checking the DB table 'authentications'
+#     and forwards the request to authorization service
+#     :param user_id: User ID
+#     :param password: User password
+#     :return: True if user is authentic, false otherwise
+#     """
+#     global cursor, AUTHORIZATION_SERVER_HOST, AUTHORIZATION_SERVER_PORT
+#     query = "SELECT COUNT(*) AS cnt FROM `authentications` WHERE user_id=%s AND user_pwd=%s;"
+#     cursor.execute(query, [user_id, password])
+#     count = cursor.fetchone()[0]
+#     return True if count >= 1 else False
+
+# def authenticate_user(user_id, password):
+#     """This function performs user authentication by checking the DB table 'authentications'
+#     and forwards the request to authorization service
+#     :param user_id: User ID
+#     :param password: User password
+#     :return: True if user is authentic, false otherwise
+#     """
+#     authentication_dict = dict()
+#     authentication_dict['1'] = 'password1'
+#     authentication_dict['2'] = 'password2'
+#     if str(user_id) in authentication_dict and authentication_dict[user_id] == password:
+#         return True
+#     else:
+#         return False
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+credentials = pika.PlainCredentials('new_user', 'password')
+parameters = pika.ConnectionParameters('149.165.169.11',
+                                       5672,
+                                       '/',
+                                       credentials)
+# connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue='authentication_queue')
 
@@ -44,7 +66,7 @@ def on_request(ch, method, props, body):
     password = req_dict['password']
     resource_id = req_dict['resourceId']
 
-    if authenticate_user(user_id, password):
+    if is_authentic_user(user_id, password):
         authorization_client = AuthorizationClient()
         message = {"id": user_id, "resource": resource_id}
         response = authorization_client.call(json.dumps(message))
